1932cf77053d65e2bd36301b18fda2de57b55183
ellaorzhu <bzhu@albany.edu>
2015-11-03 18:20:20 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+
 $(document.ready(function(){
 $("[type='checkbox']").click(function(){
 $(this).parent().toggleClass("active");
 });
+$("#btn").click(function(){
+ var bestBank = $(#bestBank");
+  bestBank.html("<h1>best bank</h1><p>this is line</p>");
+  });
 });
+$(document.body).on("click", "#nextBtn", function(){
+       $("#change").text("New Example Text");
+	   });
+	   });
