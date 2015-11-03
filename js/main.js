52545d46d6773bcc229c084d02c38dad6290f89e
ellaorzhu <bzhu@albany.edu>
2015-11-03 18:43:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $(this).parent().toggleClass("active");
 });
 $("#btn").click(function(){
  var bestBank = $("#bestBank");
-  bestBank.html("<h1>best bank</h1><p>this is line</p>");
-  });
+  bestBank.html("<h1 id='change'>best bank</h1><p>this is line</p><button type='button' id='nextBtn'>change h1</button>");
+ 
 });
 $(document.body).on("click", "#nextBtn", function(){
        $("#change").text("New Example Text");
   });
- 
+}); 
