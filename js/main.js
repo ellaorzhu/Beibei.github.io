5d2a85a683d408fed3e4cd0456fcd964205985ae
ellaorzhu <bzhu@albany.edu>
2015-11-03 19:02:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(this).parent().toggleClass("active");
 $("#btn").click(function(){
  var bestBank = $("#bestBank");
   bestBank.html("<h1 id='change'>best bank</h1><p>this is line</p><button type='button' id='nextBtn'>change h1</button>");
-   $(this).find('button').remove();
+   $("#change").hide();
 });
 $(document.body).on("click", "#nextBtn", function(){
        $("#change").text("New Example Text");
